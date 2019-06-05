11405425e8b3caa93038657b5ba72f626c9bdde3
Cao Yi <caoyi5225@sina.com>
2019-06-05 10:46:42 +0800

    add: RESET, MERGE, REMOVE dependencies action

--- src/reducers/dependencies.js
+++ src/reducers/dependencies.js
@@ -1,8 +1,33 @@
 import {getAction} from '../actions/constants';
+import { filter, findIndex } from 'ramda';
 
 const dependencies = (state = [], action) => {
     if (action.type === getAction('SET_DEPENDENCIES')) {
         return  action.payload;
+    }
+
+    if (action.type === getAction('MERGE_DEPENDENCIES')) {
+        const newState = [...state];
+        action.payload.forEach(item => {
+            const index = findIndex(dependency => 
+                dependency.output === item.output,
+                newState
+            )
+            if (index === -1) {
+                newState.push(item);
+            } else {
+                newState[index] = item; 
+            }
+        });
+        return newState;
+    }
+
+    if (action.type === getAction('REMOVE_DEPENDENCIES')) {
+        return filter(dependency =>
+            findIndex(item =>
+                dependency.output === item.output,
+                action.payload
+            ) === -1, state)
     }
 
     return state;
